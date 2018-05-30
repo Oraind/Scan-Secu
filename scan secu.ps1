3605f0cd3a29ca6641986ff4c17cd686e9404cac
dorian <dorian_gallet@outlook.fr>
2018-05-30 15:43:21 +0200

    second commit

--- scan secu.ps1
+++ scan secu.ps1
@@ -1,1 +1,15 @@
-﻿
+﻿cls
+
+$services= @( "wuauserv", "WinDefend")
+
+
+Foreach ($service in $services ) {
+
+$update =Get-Service -Name $service
+
+if ($update.Status -like "Stopped") {
+    Write-Host "le service"$update.Name"est arreter"
+    }
+else {Write-Host "le service"$update.Name"est ok"}
+}
+
